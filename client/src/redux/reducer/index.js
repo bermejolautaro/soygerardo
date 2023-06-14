8fc7887debb2b4d9c2ad872711f921d6b0000513
Agustin Sigales <asea301998@gmail.com>
2023-06-14 11:25:49 -0300

    conectando el BE

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,9 +1,18 @@
+import { GET_GAMES } from "../actions"
+
 let initialState = {allGames:[]}
 function rootReducer(state = initialState,action){
     switch(action.type){
+        case GET_GAMES:
+            return{
+            ...state,
+            allGames:action.payload
+        }
+    
         default :
         return state
     }
 }
-
 export default rootReducer
+
+console.log(initialState)
