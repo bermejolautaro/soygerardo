8f44f888f4c9ca7d29abcb60f2eb8892e3d7b96e
bermejolautaro <tarolau97@hotmail.com>
2023-06-14 23:29:25 -0300

    Finished BE

---
+++ src/helpers/genres.js
@@ -0,0 +1,31 @@
+const { API_KEY, API_URL } = process.env;
+const { Genre } = require('../db.js');
+const axios = require('axios');
+
+async function getGenresFromApi() {
+  const url = `${API_URL}/genres?key=${API_KEY}`;
+  const response = await axios.get(url);
+
+  return response.data;
+}
+
+async function seedGenresFromApi() {
+  const genresFromDb = await Genre.findAll({ limit: 1 })
+  if (genresFromDb.length <= 0) {
+    const genresFromApi = await getGenresFromApi();
+
+    try {
+      await Genre.bulkCreate(genresFromApi.results.map(x => ({
+        name: x.name
+      })))
+      console.log('Genres saved successfully from API.')
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+module.exports = {
+  getGenresFromApi,
+  seedGenresFromApi
+}
